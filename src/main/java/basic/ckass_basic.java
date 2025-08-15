081bec29b0c19e5bed421d44a1cff4d9e727e222
akashthorat12345 <thoratakash.org@gmail.com>
2025-08-15 09:11:22 +0530

    new

--- src/main/java/basic/ckass_basic.java
+++ src/main/java/basic/ckass_basic.java
@@ -3,7 +3,12 @@ package basic;
 public class ckass_basic {
 
 	system.out
-	system.out1
+	
+	public class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, Git and Eclipse!");
+	    }
+	}
 }
 
 //aksah
