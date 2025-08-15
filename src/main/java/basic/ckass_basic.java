bb9dbe031fb6b5a42606420e8096efcb486011e1
usercomvision <akash.thorat@comvision.io>
2025-08-15 07:18:52 -0700

    maza india

--- src/main/java/basic/ckass_basic.java
+++ src/main/java/basic/ckass_basic.java
@@ -8,26 +8,6 @@ public class ckass_basic {
 	    public static void main(String[] args) {
 	        System.out.println("Hello, Git and Eclipse!");
 	    }
-	}
+comvision	
+akash thorat java proect
 }
-
-public class AddNumbers {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int sum = a + b;
-        
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
-    }
-}
-
-
-package basic;
-
-public class ckass_basic {
-    public static void main(String[] args) {
-        System.out.println("Hello from Dev2!");
-    }
-}
-
-//aksah
